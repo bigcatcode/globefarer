ffcdfa1fed24a6637f270b7508501ca5bc3e9ca7
root <bigcatcode@gmail.com>
2023-11-25 12:07:43 +0000

    single header overley

--- inc/title/templates/title-content-custom.php
+++ inc/title/templates/title-content-custom.php
@@ -1,10 +1,21 @@
 <?php 
 
 $featured_img_url = get_the_post_thumbnail_url( $post->ID , 'full' );
+$header_image = get_field( 'header_image', $post->ID );
+
+$featured_img = '';
+if ( !empty($header_image)) {
+	$featured_img = $header_image['url'];
+} else {
+	$featured_img = $featured_img_url;
+}
 
 ?>
 
-<div class="qodef-m-content qodef-content-grid_ custom-page-header" style="background-image: url(<?php echo $featured_img_url; ?>);">
-	
+<div class="qodef-m-content qodef-content-grid_ custom-page-header" >
+	<?php if ( !empty($featured_img)) { ?>
+		<div class="header_overlay"></div>
+		<img src="<?php echo $featured_img; ?>">
+	<?php } ?>
 </div>
 
