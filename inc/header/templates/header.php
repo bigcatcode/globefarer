7608a501cf638512db512b47edf42db98d4b591a
root <bigcatcode@gmail.com>
2023-11-27 10:08:36 +0000

    single header overley

--- inc/header/templates/header.php
+++ inc/header/templates/header.php
@@ -3,7 +3,7 @@
 do_action( 'globefarer_action_before_page_header' );
 
 if ( is_singular( 'post' ) ) {
-	$skin = 'qodef-content-grid qodef-skin--light qodef-header-cursor--light';
+	$skin = 'class="qodef-content-grid qodef-skin--light qodef-header-cursor--light"';
 } else {
 	$skin = '';
 }
@@ -14,7 +14,7 @@ if ( is_singular( 'post' ) ) {
 	// Hook to include additional content before page header inner
 	do_action( 'globefarer_action_before_page_header_inner' );
 	?>
-	<div id="qodef-page-header-inner" class="<?php echo $skin; ?>" <?php globefarer_class_attribute( apply_filters( 'globefarer_filter_header_inner_class', array(), 'default' ) ); ?>>
+	<div id="qodef-page-header-inner" <?php echo $skin; ?> <?php globefarer_class_attribute( apply_filters( 'globefarer_filter_header_inner_class', array(), 'default' ) ); ?>>
 		<?php
 		// Include module content template
 		echo apply_filters( 'globefarer_filter_header_content_template', globefarer_get_template_part( 'header', 'templates/header-content' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
